e625271a6633f836027b8f916a3fbeefdd9e4128
Beránková <tereza.berankova@thermofisher.com>
2021-03-16 22:44:16 +0100

    new image folder

---
+++ Labs/05-counter/counter/counter.srcs/sources_1/new/top_1.vhd
@@ -0,0 +1,54 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 16.03.2021 20:47:19
+-- Design Name: 
+-- Module Name: top_1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top_1 is
+    Port ( CLK100MHZ : in STD_LOGIC;
+           BTNC : in STD_LOGIC;
+           SW : in STD_LOGIC_VECTOR (0 downto 0);
+           LED : out STD_LOGIC_VECTOR (3 downto 0);
+           CA : out STD_LOGIC;
+           CB : out STD_LOGIC;
+           CC : out STD_LOGIC;
+           CD : out STD_LOGIC;
+           CE : out STD_LOGIC;
+           CF : out STD_LOGIC;
+           CG : out STD_LOGIC;
+           AN : out STD_LOGIC_VECTOR (7 downto 0));
+end top_1;
+
+architecture Behavioral of top_1 is
+
+begin
+
+
+end Behavioral;
